006b381fa8b64b3a5515458ad29485b102ce8806
Bao Nguyen <bao.cnguyen2@gmail.com>
2018-08-05 20:33:57 -0700

    finished auth flow

---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+// do commit this file so that heroku can see this file
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
