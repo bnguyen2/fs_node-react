e63a828b8cecf83c77cafb05d16b3e749179c12a
Bao Nguyen <bao.cnguyen2@gmail.com>
2018-08-19 15:54:04 -0700

    finish webhooks

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewars.js/requireLogin');
 const requireCredits = require('../middlewars.js/requireCredits');
@@ -7,8 +10,40 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/thanks/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/thanks/:choice'); // make sure path is similar to survey template routes
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        // look at survey collection, find and update that one record
+        // also note we are not using async await (send grid is doing us a favor)
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: { $elemMatch: { email: email, responded: false } },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date(),
+          }
+        ).exec(); // update choice yes or no // update recipient who we just found in original query to true
+      })
+      .value();
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
